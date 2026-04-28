71aa7e990db62dc4b85bd2c9e7091376bf3f7579
jaewoong28 <czz9hntxtc@privaterelay.appleid.com>
2026-04-28 15:56:22 +0900

    Update weather.js

--- api/weather.js
+++ api/weather.js
@@ -1,9 +1,9 @@
 export default async function handler(req, res) {
-  const OWM_KEY = "20e4c950bcbe3acf63fa6b94be614f70"; // 임시 하드코딩
+  const OWM_KEY = "20e4c950bcbe3acf63fa6b94be614f70";
 
   try {
     const weatherRes = await fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?q=Seoul&appid=${OWM_KEY}&units=metric&lang=kr&cnt=8`
+      `https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=${OWM_KEY}&units=metric&lang=kr`
     );
     const weather = await weatherRes.json();
     res.status(200).json(weather);
